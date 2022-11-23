d0f67862a7274bb22d74f13ffcdd8f4b7e7eab5f
Bartek Banaś <bartek.banas675@gmail.com>
2022-11-23 15:21:24 +0100

    Reading data from proper directory

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ internal static class Example
     {
         CultureInfo.CurrentCulture = new CultureInfo("en-US");
         
-        const string data = "dane.txt";         //Path to data
+        const string data = "../../../data/dane.txt";         //Path to data
 
         Conditions.ReadConditions(data);
         Conditions.PrintConditions();
