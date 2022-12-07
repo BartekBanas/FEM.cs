47546527ea258b28531ef2457d868144b11287c0
Bartek Banaś <bartek.banas675@gmail.com>
2022-12-07 17:31:19 +0100

    Some system solving

--- SystemOfEquations.cs
+++ SystemOfEquations.cs
@@ -8,16 +8,16 @@ public class SystemOfEquations
     public double[] globalPvector;
     private List<Element> elements;
     private int amountOfNodes;
-    
+
     public SystemOfEquations(List<Element> elements)
-    {   
+    {
         this.elements = elements;
 
         amountOfNodes = elements[^1].nodes[2].ID;
-        
+
         system = new double[amountOfNodes, amountOfNodes];
         globalPvector = new double [amountOfNodes];
-        
+
         Aggregation();
     }
 
@@ -36,8 +36,8 @@ public class SystemOfEquations
                 {
                     system[element.nodes[i].ID - 1, element.nodes[j].ID - 1] += hmatrix[i, j];
                     system[element.nodes[i].ID - 1, element.nodes[j].ID - 1] += hbcmatrix[i, j];
-                    
                 }
+
                 globalPvector[element.nodes[i].ID - 1] += pVector[i];
             }
         }
@@ -53,234 +53,142 @@ public class SystemOfEquations
 
                 Console.Write(Math.Abs(system[i, j]).ToString("F2", CultureInfo.InvariantCulture));
                 Console.Write("\t");
-
             }
 
             Console.Write("*  ");
             Console.Write(Math.Abs(globalPvector[i]).ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine();
-            
-        }   Console.WriteLine();  
+        }
+
+        Console.WriteLine();
     }
 
     public double[] calculateSystem()
     {
+        double peak;
+        int peakID = 0;
+
         double[,] coefficents = new double [amountOfNodes, amountOfNodes + 1];
-		for (int i = 0; i < amountOfNodes; i++) {
-			coefficents[i] = new double[degree + 1];
-	}
-	
-	cout << "The degree of the system of equations is: " << degree << endl << endl;
-	cout << "Extended matrix before calculations:" << endl;
-	for (unsigned int i = 0; i < degree; i++) {
-		for (unsigned int j = 0; j < degree + 1; j++)
-		{
-			dane >> coefficents[i][j];
-			cout << coefficents[i][j] << ",\t";
-		}
 
-		cout << endl;
-	}
+        for (int i = 0; i < amountOfNodes; i++)
+        {
+            for (int j = 0; j < amountOfNodes + 1; j++)
+            {
+                coefficents[i, j] = system[i, j];
+            }
 
-	//	Partial Pivoting
-	//for (unsigned int k = 0; k < degree; k++)
-	{
-		for (unsigned int i = 0; i < degree; i++)
-		{
-			peak = abs(coefficents[i][i]);
-			for (unsigned int j = i; j < degree; j++)
-			{
-				if (peak < abs(coefficents[j][i]) && (coefficents[j][i]) != 0) {
-					peak = abs(coefficents[j][i]);
-					peakID = j;
-				}
+            coefficents[i, amountOfNodes] = globalPvector[i];
+        }
 
-			}
-			if (peakID != i) {
-				for (unsigned int j = 0; j < degree + 1; j++) {
-					swap(coefficents[i][j], coefficents[peakID][j]);
-				}
-			}
-			//cout << i + 1 << " iteration of the matrix" << endl;
-			//for (unsigned int k = 0; k < degree; k++) {                //Wypisywanie
-			//	for (unsigned int j = 0; j < degree + 1; j++) {
-			//		cout << coefficents[k][j] << ",\t";
-			//	}
-			//	cout << endl;
-			//}   cout << endl;
-		}
-	}	cout << endl << "Extended matrix after Partial Pivoting:" << endl;
-	for (unsigned int i = 0; i < degree; i++) {				//Wypisywanie
-		for (unsigned int j = 0; j < degree + 1; j++) {
-			cout << coefficents[i][j] << ",\t";
-		}
-		cout << endl;
-	}	cout << endl;
+        //	Partial Pivoting
+        for (int i = 0; i < amountOfNodes; i++)
+        {
+            peak = Math.Abs(coefficents[i, i]);
+            for (int j = i; j < amountOfNodes; j++)
+            {
+                if (peak < Math.Abs(coefficents[j, i]) && (coefficents[j, i]) != 0)
+                {
+                    peak = Math.Abs(coefficents[j, i]);
+                    peakID = j;
+                }
+            }
 
+            if (peakID != i)
+            {
+                for (int j = 0; j < amountOfNodes + 1; j++)
+                {
+                    (coefficents[i, j], coefficents[peakID, j]) = (coefficents[peakID, j], coefficents[i, j]);
+                }
+            }
+            //cout << i + 1 << " iteration of the matrix" << endl;
+            //for (unsigned int k = 0; k < degree; k++) {                //Wypisywanie
+            //	for (unsigned int j = 0; j < degree + 1; j++) {
+            //		cout << coefficents[k][j] << ",\t";
+            //	}
+            //	cout << endl;
+            //}   cout << endl;
+        }
 
+        Console.WriteLine("\nExtended matrix after Partial Pivoting:\n");
+        for (int i = 0; i < amountOfNodes; i++)
+        {
+            //Wypisywanie
+            for (int j = 0; j < amountOfNodes + 1; j++)
+            {
+                Console.Write($"{coefficents[i, j]},\t");
+            }
 
-	double** multiplier = new double* [degree];
-	for (unsigned int i = 1; i < degree; i++) {
-		multiplier[i] = new double[2];
-		multiplier[i][0] = coefficents[i][0] / coefficents[0][0];
-		multiplier[i][1] = coefficents[i][1] / coefficents[1][1];
-	}	cout << endl;
+            Console.WriteLine();
+        }
 
-	for (unsigned int k = 0; k < degree - 1; k++)
-	{
-		for (unsigned int i = k + 1; i < degree; i++) {			//Operacje
-			for (unsigned int j = k; j <= degree; j++) {
-				coefficents[i][j] -= (coefficents[k][j] * multiplier[i][0]);
-			}
-		}
-
-		for (unsigned int i = k + 2; i < degree; i++) {			//Mnożniki
-			multiplier[i][0] = coefficents[i][k + 1] / coefficents[k + 1][k + 1];
-		}
-	}
-
-	cout << "Extended matrix after the first stage of calculations:" << endl;
-	for (unsigned int i = 0; i < degree; i++) {				//Wypisywanie
-		for (unsigned int j = 0; j < degree + 1; j++) {
-			cout << coefficents[i][j] << ",\t";
-		}
-		cout << endl;
-	}	cout << endl;
-
-	double* xi = new double[degree];
-	double* sum = new double[degree];
-	xi[degree - 1] = coefficents[degree - 1][degree] / coefficents[degree - 1][degree - 1];
-	cout << "Solution of the system of equations:" << endl;
-
-	for (int i = degree - 2; i >= 0; i--) {
-		sum[i] = 0;
-		for (unsigned int k = i + 1; k < degree; k++) {
-			sum[i] += (coefficents[i][k] * xi[k]);
-		}
-
-		xi[i] = (coefficents[i][degree] - sum[i]) / coefficents[i][i];
-	}
-
-	for (unsigned int i = 0; i < degree; i++)
-	{
-		cout << "x" << i + 1 << " = " << xi[i] << endl;
-	}
-
-	//	Eliminacja Gaussa-Crouta
+        Console.WriteLine();
 
 
-	dane2 >> degree2;
-	unsigned int* Xs = new unsigned int[degree2];
-	for (unsigned int i = 0; i < degree2; i++) {
-		Xs[i] = i;
-	}
+        double[,] multiplier = new double[amountOfNodes, 2];
+        for (int i = 1; i < amountOfNodes; i++)
+        {
+            multiplier[i, 0] = coefficents[i, 0] / coefficents[0, 0];
+            multiplier[i, 1] = coefficents[i, 1] / coefficents[1, 1];
+        }
 
-	double** secondArray = new double* [degree2];
-	for (unsigned int i = 0; i < degree2; i++) {
-		secondArray[i] = new double[degree2 + 1];
-	}
+        Console.WriteLine();
 
+        for (int k = 0; k < amountOfNodes - 1; k++)
+        {
+            for (int i = k + 1; i < amountOfNodes; i++)
+            {
+                //Operacje
+                for (int j = k; j <= amountOfNodes; j++)
+                {
+                    coefficents[i, j] -= (coefficents[k, j] * multiplier[i, 0]);
+                }
+            }
 
-	cout << endl << endl << "The degree of the second system of equations is: " << degree2 << endl << endl;
-	cout << "Extended second matrix before calculations:" << endl;
-	for (unsigned int i = 0; i < degree2; i++) {
-		for (unsigned int j = 0; j < degree2 + 1; j++)
-		{
-			dane2 >> secondArray[i][j];
-			cout << secondArray[i][j] << ",\t";
-		}
-		cout << endl;
-	}
-	
-	//for (unsigned int k = 0; k < degree2; k++)
-	{
-		for (unsigned int i = 0; i < degree2; i++)
-		{
-			peak = abs(secondArray[i][i]), peakID = i;
-			for (unsigned int j = i; j < degree2; j++)
-			{
-				if (peak < abs(secondArray[i][j]) && (secondArray[j][j]) != 0) {
-					peak = abs(secondArray[i][j]);
-					peakID = j;
-				}
-			}
-			if (peakID != i) {
-				swap(Xs[peakID], Xs[i]);
-				for (unsigned int j = 0; j < degree2; j++) {
-					swap(secondArray[j][i], secondArray[j][peakID]);
-				}	/*
-				cout << "Columns " << peakID + 1 << " and " << i + 1 << " have been swapped" << endl;
-				for (unsigned int k = 0; k < degree2; k++) {                //Wypisywanie
-					for (unsigned int j = 0; j < degree2 + 1; j++) {
-						cout << secondArray[k][j] << ",\t";
-					}
-					cout << endl;
-				}   cout << endl;	*/
-			}
-		}
-	}	cout << endl << "Extended second matrix after eliminating zeros from the diagonal:" << endl;
-	for (unsigned int i = 0; i < degree2; i++) {				//Wypisywanie
-		for (unsigned int j = 0; j < degree2 + 1; j++) {
-			cout << secondArray[i][j] << ",\t";
-		}
-		cout << endl;
-	}	cout << endl;
+            for (int i = k + 2; i < amountOfNodes; i++)
+            {
+                //Mnożniki
+                multiplier[i, 0] = coefficents[i, k + 1] / coefficents[k + 1, k + 1];
+            }
+        }
 
-	double** multiplier2 = new double* [degree2];
-	for (unsigned int i = 1; i < degree2; i++) {
-		multiplier2[i] = new double[2];
-		multiplier2[i][0] = secondArray[i][0] / secondArray[0][0];
-		multiplier2[i][1] = secondArray[i][1] / secondArray[1][1];
-	}	cout << endl;
+        Console.WriteLine("Extended matrix after the first stage of calculations:");
+        for (int i = 0; i < amountOfNodes; i++)
+        {
+            //Wypisywanie
+            for (int j = 0; j < amountOfNodes + 1; j++)
+            {
+                Console.Write($"{coefficents[i, j]},\t");
+            }
 
-	for (unsigned int k = 0; k < degree2 - 1; k++)
-	{
-		for (unsigned int i = k + 1; i < degree2; i++) {			//Operacje
-			for (unsigned int j = k; j <= degree2; j++) {
-				secondArray[i][j] -= (secondArray[k][j] * multiplier2[i][0]);
-			}
-		}
+            Console.WriteLine();
+        }
 
-		for (unsigned int i = k + 2; i < degree2; i++) {			//Mnożniki
-			multiplier2[i][0] = secondArray[i][k + 1] / secondArray[k + 1][k + 1];
-		}
-	}
+        Console.WriteLine();
 
-	cout << "Extended matrix after the first stage of calculations:" << endl;
-	for (unsigned int i = 0; i < degree2; i++) {				//Wypisywanie
-		for (unsigned int j = 0; j < degree2 + 1; j++) {
-			cout << secondArray[i][j] << ",\t";
-		}
-		cout << endl;
-	}	cout << endl;
+        double[] xi = new double[amountOfNodes];
+        double[] sum = new double[amountOfNodes];
+        xi[amountOfNodes - 1] = coefficents[amountOfNodes - 1, amountOfNodes] /
+                                coefficents[amountOfNodes - 1, amountOfNodes - 1];
+        Console.WriteLine("Solution of the system of equations:");
 
-	double* xi2 = new double[degree2];
-	double* sum2 = new double[degree2];
-	xi2[degree2 - 1] = secondArray[degree2 - 1][degree2] / secondArray[degree2 - 1][degree2 - 1];
-	
+        for (int i = amountOfNodes - 2; i >= 0; i--)
+        {
+            sum[i] = 0;
+            for (int k = i + 1; k < amountOfNodes; k++)
+            {
+                sum[i] += coefficents[i, k] * xi[k];
+            }
 
-	for (int i = degree2 - 2; i >= 0; i--) {
-		sum2[i] = 0;
-		for (unsigned int k = i + 1; k < degree2; k++) {
-			sum2[i] += (secondArray[i][k] * xi2[k]);
-		}
+            xi[i] = (coefficents[i, amountOfNodes] - sum[i]) / coefficents[i, i];
+        }
 
-		xi2[i] = (secondArray[i][degree2] - sum2[i]) / secondArray[i][i];
-	}
+        for (int i = 0; i < amountOfNodes; i++)
+        {
+            Console.WriteLine($"x{i + 1} = {xi[i]}");
+        }
 
-	for (unsigned int i = 0; i < degree2; i++)
-	{
-		cout << Xs[i] + 1 << "  ";
-	}	cout << endl;
+        //	Eliminacja Gaussa-Crouta maybe someday
 
-	for (unsigned int i = 0; i < degree2; i++) {		//Przypisywanie poprawnych wartości zmiennych
-		sum2[Xs[i]] = xi2[i];
-	}
-
-	cout << endl << "Solution of the system of equations:" << endl;
-	for (unsigned int i = 0; i < degree2; i++)
-	{
-		cout << "x" << i + 1 << " = " << sum2[i] << endl;
-	}
+        return xi;
     }
 }
