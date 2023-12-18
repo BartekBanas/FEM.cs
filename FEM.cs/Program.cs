65ed56e6e42f38157706318355dd5ffd348900e5
Bartłomiej Banaś <bartek.banas675@gmail.com>
2023-12-19 00:55:39 +0100

    Refactor input data files

--- FEM.cs/Program.cs
+++ FEM.cs/Program.cs
@@ -5,7 +5,7 @@ CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
 var simulationModel = new SimulationModel();
 
-const string pathToDataFile = "../../../../data/dane.txt";
+const string pathToDataFile = "../../../../data/example_4x4_1.txt";
 
 simulationModel.Initialize(pathToDataFile);
 
