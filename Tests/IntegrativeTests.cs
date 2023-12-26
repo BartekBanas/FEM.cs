a93f4426699c7e9d8d2ac47d88d8aac03e92b05e
Bartłomiej Banaś <bartek.banas675@gmail.com>
2023-12-26 19:54:38 +0100

    Add an integrative test

---
+++ Tests/IntegrativeTests.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+using FEM.cs;
+using Xunit;
+
+namespace Tests;
+
+public class IntegrativeTests
+{
+    [Fact]
+    public void Test4X4Matrix()
+    {
+        // Arrange
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        var simulationModel = new SimulationModel();
+
+        const string pathToDataFile = "../../../../data/example_4x4_1.txt";
+
+        simulationModel.Initialize(pathToDataFile);
+        UniversalElement.Initialize(4);
+        var simulation = new Simulation(simulationModel);
+        
+        // Act
+        simulation.RunSimulation();
+        
+        // Assert
+        for (int i = 0; i <= 10; i++)
+        {
+            var expected = File.ReadAllText("../../../../results/" + $"Data_{i:D3}.vtk");
+            var received = File.ReadAllText("../../../testOutput/" + $"Data_{i:D3}.test");
+
+            Assert.Equal(received,expected);
+        }
+    }
+}
