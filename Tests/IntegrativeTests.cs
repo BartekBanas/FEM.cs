6bf14380d23ba20561464199255eb39354caf9ef
Bartłomiej Banaś <bartek.banas675@gmail.com>
2023-12-26 20:19:06 +0100

    Add test data for 31x31 matrix

--- Tests/IntegrativeTests.cs
+++ Tests/IntegrativeTests.cs
@@ -37,7 +37,7 @@ public class IntegrativeTests
     {
         // Arrange
         CultureInfo.CurrentCulture = new CultureInfo("en-US");
-        const string pathToDataFile = "../../../../data/example_4x4_2.txt";
+        const string pathToDataFile = "../../../testInput/example_4x4_2.txt";
         
         UniversalElement.Initialize(4);
 
@@ -53,6 +53,32 @@ public class IntegrativeTests
         {
             var expected = File.ReadAllText("../../../../results/" + $"Data_{i:D3}.vtk");
             var received = File.ReadAllText("../../../testOutput_4x4_2/" + $"Data_{i:D3}.test");
+
+            Assert.Equal(received,expected);
+        }
+    }
+    
+    [Fact]
+    public void Test31X31Matrix()
+    {
+        // Arrange
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        const string pathToDataFile = "../../../../data/example_31x31_1.txt";
+        
+        UniversalElement.Initialize(4);
+
+        var simulationModel = new SimulationModel();
+        simulationModel.Initialize(pathToDataFile);
+        var simulation = new Simulation(simulationModel);
+        
+        // Act
+        simulation.RunSimulation();
+        
+        // Assert
+        for (int i = 0; i <= 20; i++)
+        {
+            var expected = File.ReadAllText("../../../../results/" + $"Data_{i:D3}.vtk");
+            var received = File.ReadAllText("../../../testOutput_31x31_1/" + $"Data_{i:D3}.test");
 
             Assert.Equal(received,expected);
         }
